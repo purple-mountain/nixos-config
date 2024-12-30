97bf49d90f591919fd0134c5751899f7a2c696d7
purple-mountain <lmwrong92@gmail.com>
2024-12-30 18:44:10 +0000

    added ghostty flake

--- flake.nix
+++ flake.nix
@@ -8,20 +8,30 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    ghostty = {
+      url = "github:ghostty-org/ghostty";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs = { self, nixpkgs, ghostty, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in
     {
       nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-          specialArgs = {inherit inputs;};
+          specialArgs = {
+            inherit inputs;
+          };
           modules = [ 
             ./hosts/default/configuration.nix
-            #./configuration.nix 
             inputs.home-manager.nixosModules.default
+            {
+              environment.systemPackages = [
+                ghostty.packages.${system}.default
+              ];
+            }
           ];
         };
     };
